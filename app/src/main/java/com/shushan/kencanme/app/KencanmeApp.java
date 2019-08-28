307af691abf2537d75240b08fd3e8470c9774e27
llxqb <liuli7168165>
2019-08-28 16:15:37 +0800

    去掉了热更新

--- app/src/main/java/com/shushan/kencanme/app/KencanmeApp.java
+++ app/src/main/java/com/shushan/kencanme/app/KencanmeApp.java
@@ -12,9 +12,6 @@ import com.shushan.kencanme.app.entity.Constants.ServerConstant;
 import com.shushan.kencanme.app.mvp.ui.activity.rongCloud.CustomizeMessage;
 import com.shushan.kencanme.app.mvp.ui.activity.rongCloud.CustomizeMessageItemProvider;
 import com.squareup.leakcanary.LeakCanary;
-import com.taobao.sophix.PatchStatus;
-import com.taobao.sophix.SophixManager;
-import com.taobao.sophix.listener.PatchLoadStatusListener;
 import com.umeng.commonsdk.UMConfigure;
 
 import javax.inject.Inject;
@@ -43,12 +40,6 @@ public class KencanmeApp extends Application {
         return mAppComponent;
     }
 
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        //初始化热修复
-        initHotfix();
-    }
 
     @Override
     public void onCreate() {
@@ -56,8 +47,6 @@ public class KencanmeApp extends Application {
         mContext = this.getApplicationContext();
         mAppComponent = DaggerAppComponent.builder().appModule(new AppModule(this)).build();
         mAppComponent.inject(this);//必须有
-        // queryAndLoadNewPatch不可放在attachBaseContext 中，否则无网络权限，建议放在后面任意时刻，如onCreate中
-        SophixManager.getInstance().queryAndLoadNewPatch();
         //初始化内存泄漏检查工具
         initLeaks();
         //初始化融云
@@ -78,41 +67,6 @@ public class KencanmeApp extends Application {
         }
         LeakCanary.install(this);
     }
-
-    /**
-     * 初始化阿里移动热更新
-     */
-    private void initHotfix() {
-        String appVersion;
-        try {
-            appVersion = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
-        } catch (Exception e) {
-            appVersion = "1.0.0";
-        }
-        // initialize必须放在attachBaseContext最前面，初始化代码直接写在Application类里面，切勿封装到其他类。
-        SophixManager.getInstance().setContext(this)
-                .setAppVersion(appVersion)
-                .setAesKey(null)
-                .setEnableDebug(true)
-                .setPatchLoadStatusStub(new PatchLoadStatusListener() {
-                    @Override
-                    public void onLoad(final int mode, final int code, final String info, final int handlePatchVersion) {
-                        // 补丁加载回调通知
-                        if (code == PatchStatus.CODE_LOAD_SUCCESS) {
-                            // 表明补丁加载成功
-//                            ToastUtil.showToast(mContext,"表明补丁加载成功");
-                        } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
-//                            ToastUtil.showToast(mContext,"表明新补丁生效需要重启. 开发者可提示用户或者强制重启");
-                            // 表明新补丁生效需要重启. 开发者可提示用户或者强制重启;
-                            // 建议: 用户可以监听进入后台事件, 然后调用killProcessSafely自杀，以此加快应用补丁，详见1.3.2.3
-                        } else {
-//                            ToastUtil.showToast(mContext,"error()");
-                            // 其它错误信息, 查看PatchStatus类说明
-                        }
-                    }
-                }).initialize();
-    }
-
 
     /**
      * OnCreate 会被多个进程重入，这段保护代码，确保只有您需要使用 RongIM 的进程和 Push 进程执行了 init。
